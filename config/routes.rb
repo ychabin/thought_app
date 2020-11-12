8910e00516b38121306f102d863b367289b82aec
ychabin <chabin043@icloud.com>
2020-11-12 18:24:16 +0900

    thought削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'thoughts#index'
-    resources :thoughts, only: [ :index, :new, :create, :show]
+    resources :thoughts, only: [ :index, :new, :create, :show, :destroy]
     resources :users, only: [:show]
 end
